71b223e8dabf12d5f8418a54b2c9c9343af61c6f
sumon2807 <sharifulbd911@gmail.com>
2024-02-02 22:43:00 +0600

    navbar is full responsive

---
+++ src/Pages/Shared/Container/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Container = ({children}) => {
+    return (
+        <div className='max-w-[2520px] max-auto xl:px-10 md:px-5 sm:px-2 px-4'>
+            {children}
+        </div>
+    );
+};
+
+export default Container;
--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,6 @@ import { router } from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className=' max-w-7xl mx-auto'>
       <RouterProvider router={router} />
-    </div>
   </React.StrictMode>,
 )
